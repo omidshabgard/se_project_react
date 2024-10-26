a184cb33eae56d9802e7f257df478d7635180f4f
omidshabgard <omidsangar@gmail.com>
2024-10-26 11:37:14 -0400

    new changes added

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,70 +1,71 @@
-const baseUrl = "http://localhost:3001";
+// Import BASE_URL from constants.js
+import { BASE_URL } from './constants';
 
 function checkResponse(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    return Promise.reject(`Error ${res.status}`);
-  }
+	if (res.ok) {
+		return res.json();
+	} else {
+		return Promise.reject(`Error ${res.status}`);
+	}
 }
 
 function getItems() {
-  return fetch(`${baseUrl}/items`).then((res) => checkResponse(res));
+	return fetch(`${BASE_URL}/items`).then((res) => checkResponse(res));
 }
 
 function postItems(generatedData) {
-  const token = localStorage.getItem("token");
+	const token = localStorage.getItem('token');
 
-  return fetch(`${baseUrl}/items`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(generatedData),
-  }).then((res) => checkResponse(res));
+	return fetch(`${BASE_URL}/items`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token}`,
+		},
+		body: JSON.stringify(generatedData),
+	}).then((res) => checkResponse(res));
 }
 
 function deleteItem(id) {
-  if (id) {
-    const token = localStorage.getItem("token");
+	if (id) {
+		const token = localStorage.getItem('token');
 
-    return fetch(`${baseUrl}/items/${id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }).then((res) => checkResponse(res));
-  }
+		return fetch(`${BASE_URL}/items/${id}`, {
+			method: 'DELETE',
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		}).then((res) => checkResponse(res));
+	}
 }
 
 function likeItem(itemId) {
-  const token = localStorage.getItem("token");
+	const token = localStorage.getItem('token');
 
-  return fetch(`${baseUrl}/items/${itemId}/likes`, {
-    method: "PUT", 
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  }).then((res) => checkResponse(res));
+	return fetch(`${BASE_URL}/items/${itemId}/likes`, {
+		method: 'PUT',
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	}).then((res) => checkResponse(res));
 }
 
 function dislikeItem(itemId) {
-  const token = localStorage.getItem("token");
+	const token = localStorage.getItem('token');
 
-  return fetch(`${baseUrl}/items/${itemId}/likes`, {
-    method: "DELETE", 
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  }).then((res) => checkResponse(res));
+	return fetch(`${BASE_URL}/items/${itemId}/likes`, {
+		method: 'DELETE',
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	}).then((res) => checkResponse(res));
 }
 
 export {
-  deleteItem,
-  postItems,
-  getItems,
-  checkResponse,
-  likeItem,
-  dislikeItem,
+	deleteItem,
+	postItems,
+	getItems,
+	checkResponse,
+	likeItem,
+	dislikeItem,
 };
